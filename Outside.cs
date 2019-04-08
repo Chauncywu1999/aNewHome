31b5dc69c6cb9c81a92a3708fdca5e48c7d8b4ee
Chauncywu <44538024+Chauncywu1999@users.noreply.github.com>
2019-04-08 23:22:50 +0800

    Update Outside.cs

--- Outside.cs
+++ Outside.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -41,6 +41,24 @@ namespace aHome
             get
             {
                 return base.Description + " You see " + DoorDescription + ".";
+            }
+        }
+    }
+
+    class OutsideWithHidingPlace:Outside,IHidingPlace
+    {
+        public OutsideWithHidingPlace(string name,bool hot,string hidingPlaceName):base(name,hot)
+        {
+            HidingPlaceName = hidingPlaceName;
+        }
+
+        public string HidingPlaceName { get; private set; }
+
+        public override string Description
+        {
+            get
+            {
+                return base.Description + " Someone could hide " + HidingPlaceName + ".";
             }
         }
     }
