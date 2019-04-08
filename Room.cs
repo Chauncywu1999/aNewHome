7d265b53628e82d8e6b574f028c548a2d81358ed
Chauncywu <44538024+Chauncywu1999@users.noreply.github.com>
2019-04-08 13:02:10 +0800

    create the new file

---
+++ Room.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aHome
+{
+    class Room:Location
+    {
+        private string decoration;
+
+        public Room(string name,string decoration):base(name)
+        {
+            this.decoration = decoration;
+        }
+
+        public override string Description
+        {
+            get { return base.Description + " You see " + decoration + "."; }
+        }
+    }
+
+    class RoomWithDoor:Room,IHasExteriorDoor
+    {
+        public RoomWithDoor(string name,string decoration,string doorDescription):base(name,decoration)
+        {
+            DoorDescription = doorDescription;
+        }
+
+        public string DoorDescription { get;private set; }
+
+        public Location DoorLocation { get; set; }
+    }
+}
